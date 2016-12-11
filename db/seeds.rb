d80a70f98761f27fd0b8fab334ef9b4f8069c9a5
Steve Hull <p.witty@gmail.com>
2016-12-11 13:48:33 -0800

    Updating image urls, add the initial event

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Event.find_or_create_by name: "Women's March On Washington",
-  location_name: "2 Lincoln Memorial Cir NW, Washington, DC 20037",
-  lat: 38.8892686,
-  lon: -77.050176,
-  starts_at: Time.parse("Saturday, January 21, 2017 at 10 AM EST"),
-  timezone: "EST",
-  image_url: "https://s18.postimg.org/58x863zh5/Women_s_Marchon_Washington_cover_168e3ad8_112e_432.jpg",
+
+Event.find_or_create_by name: "Women and Allies- Nationwide Protest",
+  location_name: "Justin Herman Plaza, San Francisco, CA",
+  lat: 37.7951195,
+  lon: -122.3952007,
+  starts_at: Time.parse("Monday, December 12, 2016 at 3 PM PST"),
+  timezone: "PST",
+  image_url: "https://s17.postimg.org/fx8gw48qn/ftf_event_detail_01.jpg",
   description: <<-EOF
-On January 21, 2017 we will unite in Washington, DC for the Women’s March on Washington. We stand together in solidarity with our partners and children for the protection of our rights, our safety, our health, and our families -- recognizing that our vibrant and diverse communities are the strength of our country. 
-
-The rhetoric of the past election cycle has insulted, demonized, and threatened many of us--women, immigrants of all statuses, those with diverse religious faiths particularly Muslim, people who identify as LGBTQIA, Native and Indigenous people, Black and Brown people, people with disabilities, the economically impoverished and survivors of sexual assault. We are confronted with the question of how to move forward in the face of national and international concern and fear.
-
-In the spirit of democracy and honoring the champions of human rights, dignity, and justice who have come before us, we join in diversity to show our presence in numbers too great to ignore. The Women’s March on Washington will send a bold message to our new administration on their first day in office, and to the world that women's rights are human rights. We stand together, recognizing that defending the most marginalized among us is defending all of us. 
-
-We support the advocacy and resistance movements that reflect our multiple and intersecting identities. We call on all defenders of human rights to join us. This march is the first step towards unifying our communities, grounded in new relationships, to create change from the grassroots level up. We will not rest until women have parity and equity at all levels of leadership in society. We work peacefully while recognizing there is no true peace without justice and equity for all. HEAR OUR VOICE.
-
-//////////////////////////////////////////////////////////////////////////////////
-
-This is an INCLUSIVE march, and EVERYONE who supports women's rights are welcome.
-
-PLEASE SHARE, we need to spread the word everywhere! And there are groups forming for EVERY STATE, so search for your local group as well!
-
-We especially need HOSTS and ORGANIZERS from marginalized communities for this to be truly inclusive, please DM your state administrator to get involved!
-
-///////////////////////////////////////////////////////////////////////////////////
+Join peaceful Women and Allies- Nationwide Protest - Dec. 12th NYC all over the US on December 12th and in SAN FRANCISCO! (if you are not in SAN FRANCISCO see times and dates for each city) or sign the petition online. 
 EOF
 
 Event.find_or_create_by name: "Black Lives Matter Bay Area General Meeting",
@@ -38,7 +23,7 @@ Event.find_or_create_by name: "Black Lives Matter Bay Area General Meeting",
   lon: -122.2746455,
   starts_at: Time.parse("Thursday, December 15, 2016 at 7 PM PST"),
   timezone: "Pacific Time (US & Canada)",
-  image_url: "https://scontent.fsjc1-3.fna.fbcdn.net/v/t1.0-9/15267588_1463367847021235_7917928950397929312_n.jpg?oh=ae6cd5f9811a7cbf912d59a86daf6ebe&oe=58BFFF5C",
+  image_url: "https://s29.postimg.org/5hvhp9jt3/ftf_event_detail_02.jpg",
   description: <<-EOF
 You are invited to join the Black Lives Matter- Bay Area general body meeting next week on Thursday, December 15th 7-9PM at Qilombo. This meeting will be dedicated towards political education around the Movement for Black Lives Policy Platform. ***Black and African Descent Folks Only***
 EOF
@@ -50,7 +35,7 @@ Event.find_or_create_by name: "Intro to Standing Up for Racial Justice Meeting",
   lon: -122.3754129,
   starts_at: Time.parse("Wednesday, January 11, 2017 at 12AM PST"),
   timezone: "Pacific Time (US & Canada)",
-  image_url: "https://scontent.fsjc1-3.fna.fbcdn.net/v/t1.0-9/15179204_1135410496573112_5823490839462612973_n.jpg?oh=506580cceea4d0acc4f33bcb11f68a70&oe=58F6EB00",
+  image_url: "https://s27.postimg.org/wuh75z3pv/ftf_event_detail_03.jpg",
   description: <<-EOF
 SURJ is a national network of groups and individuals organizing White people for racial justice. Through community organizing, mobilizing, and education, SURJ moves White people to act as part of a multi-racial majority for justice with passion and accountability. 
 
@@ -64,7 +49,7 @@ Event.find_or_create_by name: "Resist Fascism & Reclaim MLK - 120 Hours of Direc
   lon: -122.4222351,
   starts_at: Time.parse("Monday, January 16, 2017 at 12AM PST"),
   timezone: "Pacific Time (US & Canada)",
-  image_url: "https://s17.postimg.org/bxmodum4v/atpt.jpg",
+  image_url: "https://s17.postimg.org/uwlz6g4n3/ftf_event_detail_04.jpg",
   description: <<-EOF
 This year, we will begin our week of action with the Reclaiming King's Radical Legacy March. We will then launch into 120 hours of autonomous direct action in resistance to the fascist Trump agenda.
 
@@ -79,7 +64,7 @@ Event.find_or_create_by name: "Women’s March San Francisco",
   lon: -122.4222351,
   starts_at: Time.parse("Saturday, January 21, 2017 at 4 PM PST"),
   timezone: "Pacific Time (US & Canada)",
-  image_url: "https://scontent.fsjc1-3.fna.fbcdn.net/v/t31.0-8/15370074_600968753431751_7964634471332596057_o.jpg?oh=15d6a9cfee67a42020c86f582c27bb9d&oe=58F38087",
+  image_url: "https://s28.postimg.org/apvd6tn2l/ftf_event_detail_05.jpg",
   description: <<-EOF
 The Women's March is a national movement to unify and empower everyone who stands for human rights, civil liberties, and social justice for all.
 
